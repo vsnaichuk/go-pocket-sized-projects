723262876aaa22e55a80161d9454a0518ef037aa
vsnaichuk <v.snaichuk@gmail.com>
2023-12-30 15:11:29 +0200

    feat(main): add usage examples for pocketlog

--- chapter_03_logger/main.go
+++ chapter_03_logger/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"learngo-pockets/logger/pocketlog"
+	"os"
+	"time"
+  "learngo-pockets/pocketlog"
 )
 
 func main() {
-	lvl := pocketlog.LevelDebug
-	fmt.Printf("Level: %v\n", lvl)
+		// Instantiating a new logger and calling a few methods to showcase usage
+    lgr := pocketlog.New(pocketlog.LevelInfo, os.Stdout)
+		lgr.Infof("A little copying is better than a little dependency.")
+    lgr.Errorf("Errors are values. Documentation is for %s.", "users")
+    lgr.Debugf("Make the zero (%d) value useful.", 0)
+    lgr.Infof("Hallo, %d %v", 2024, time.Now())
 }
